802d730e87108c088f9436cf0e49f143239ae80a
flaxen-infosoft <akshaythakur0308@gmail.com>
2022-11-19 12:29:45 +0530

    fragment_employee_update_client

--- app/src/main/java/com/flaxeninfosoft/guptaoffset/views/employee/fragments/EmployeeUpdateClientFragment.java
+++ app/src/main/java/com/flaxeninfosoft/guptaoffset/views/employee/fragments/EmployeeUpdateClientFragment.java
@@ -28,6 +28,6 @@ public class EmployeeUpdateClientFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_employee_update_cloient, container, false);
+        return inflater.inflate(R.layout.fragment_employee_update_client, container, false);
     }
 }
