858c5c6c2ddb798608faf94b6da565e937b0afa0
rahul <rahulmauryarob@gmail.com>
2022-11-23 14:16:59 +0530

    Implement Attendance module

--- app/src/main/java/com/flaxeninfosoft/guptaoffset/models/Attendance.java
+++ app/src/main/java/com/flaxeninfosoft/guptaoffset/models/Attendance.java
@@ -1,14 +1,52 @@
 package com.flaxeninfosoft.guptaoffset.models;
-//TODO
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class Attendance {
 
+    @SerializedName("id")
+    @Expose
     private Long id;
 
-    public Long getId() {
-        return id;
+    @SerializedName("time_in")
+    @Expose
+    private String timeIn;
+
+    @SerializedName("time_out")
+    @Expose
+    private String timeOut;
+
+    @SerializedName("date")
+    @Expose
+    private String date;
+
+    @SerializedName("empId")
+    @Expose
+    private Long empId;
+
+    public Attendance(){
+    }
+    public Long getId(){return id;}
+
+    public void setId(Long id) {this.id = id;}
+
+    public String getTimeIn() {
+        return timeIn;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public void setTimeIn(String timeIn) {this.timeIn = timeIn;}
+
+    public String setTimeOut() {return timeOut;}
+
+    public void setTimeOut(String timeOut) {this.timeOut =timeOut;}
+
+    public String getDate(){return  date;}
+
+    public void setDate(String date) {this.date = date;}
+
+    public Long getEmpId() {return empId;}
+
+    public void setEmpId(Long empId) {
+        this.empId = empId;}
 }
