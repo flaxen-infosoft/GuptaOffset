0b1b9b8044b043329501efb6d453d0df934cff95
rahul <rahulmauryarob@gmail.com>
2022-12-22 11:10:15 +0530

    Add OrderDetails fragments model

--- app/src/main/java/com/flaxeninfosoft/guptaoffset/views/employee/fragments/EmployeeAddAttendanceFragment.java
+++ app/src/main/java/com/flaxeninfosoft/guptaoffset/views/employee/fragments/EmployeeAddAttendanceFragment.java
@@ -14,4 +14,25 @@ import com.flaxeninfosoft.guptaoffset.databinding.FragmentEmployeeAddAttendanceB
 import com.flaxeninfosoft.guptaoffset.viewModels.EmployeeViewModel;
 
 
+public class EmployeeAddAttendanceFragment extends Fragment {
+    private FragmentEmployeeAddAttendanceBinding binding;
+    private EmployeeViewModel viewModel;
+    ProgressDialog progressDialog;
+
+    public EmployeeAddAttendanceFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_employee_add_attendance, container, false);
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setTitle("Loading.....");
+        progressDialog.setMessage("Getting Attendance");
+        progressDialog.setCancelable(false);
+
+        viewModel.fetchEmployeeTodaysAttendance().observe(getViewLifecycleOwner(), this::setAttendance);
+    }
 }
