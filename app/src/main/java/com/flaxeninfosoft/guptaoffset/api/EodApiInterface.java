70351424fbc2a8cb5b5ecce3d9c011ba3825de20
Pankaj Bairwa <bairwapankaj25@gmail.com>
2022-12-23 10:53:40 +0530

    bugfix in EodApiInterface

--- app/src/main/java/com/flaxeninfosoft/guptaoffset/api/EodApiInterface.java
+++ app/src/main/java/com/flaxeninfosoft/guptaoffset/api/EodApiInterface.java
@@ -1,5 +1,6 @@
 package com.flaxeninfosoft.guptaoffset.api;
 
+import com.flaxeninfosoft.guptaoffset.models.Eod;
 import com.flaxeninfosoft.guptaoffset.utils.ApiEndpoints;
 import com.flaxeninfosoft.guptaoffset.utils.Constants;
 
@@ -14,19 +15,19 @@ import retrofit2.http.Query;
 public interface EodApiInterface {
 
     @GET(ApiEndpoints.GET_EOD_BY_ID)
-    Call<EOD> getEodById(@Query(Constants.EOD_ID) Long eodId);
+    Call<Eod> getEodById(@Query(Constants.EOD_ID) Long eodId);
 
     @POST(ApiEndpoints.ADD_EOD)
-    Call<EOD> addEod(@Body EOD eod);
+    Call<Eod> addEod(@Body Eod eod);
 
     @GET(ApiEndpoints.GET_EMPLOYEE_ALL_EOD_BY_ID)
-    Call<List<EOD>> getEmployeeAllEodsById(@Query(Constants.EMPLOYEE_ID) Long empId);
+    Call<List<Eod>> getEmployeeAllEodsById(@Query(Constants.EMPLOYEE_ID) Long empId);
 
     @GET(ApiEndpoints.GET_ALL_EOD)
-    Call<List<EOD>> getAllEods();
+    Call<List<Eod>> getAllEods();
 
     @GET(ApiEndpoints.GET_EMPLOYEE_TODAY_EOD)
-    Call<EOD> getEmployeeTodaysEod(@Query(Constants.EMPLOYEE_ID) Long empId);
+    Call<Eod> getEmployeeTodaysEod(@Query(Constants.EMPLOYEE_ID) Long empId);
 
 }
 
