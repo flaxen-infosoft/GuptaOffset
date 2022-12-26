aac59b844cab2f21a3f94a9a9ca30eb36420fce6
Pankaj Bairwa <bairwapankaj25@gmail.com>
2022-12-26 17:22:02 +0530

    Added and Implemented LeaveRecyclerAdapter. Implemented EmployeeAddClientFragment. Added single_leave_card. Changes in single_expense_card.

---
+++ app/src/main/java/com/flaxeninfosoft/guptaoffset/adapters/LeaveRecyclerAdapter.java
@@ -0,0 +1,69 @@
+package com.flaxeninfosoft.guptaoffset.adapters;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.flaxeninfosoft.guptaoffset.R;
+import com.flaxeninfosoft.guptaoffset.databinding.SingleLeaveCardBinding;
+import com.flaxeninfosoft.guptaoffset.models.Leave;
+
+import java.util.List;
+
+public class LeaveRecyclerAdapter extends RecyclerView.Adapter<LeaveRecyclerAdapter.LeaveCardViewHolder> {
+
+    private List<Leave> leaveList;
+    private SingleLeaveCardOnClickListener leaveCardOnClickListener;
+
+    public LeaveRecyclerAdapter(List<Leave> leaveList, SingleLeaveCardOnClickListener onClickListener) {
+        this.leaveList = leaveList;
+        this.leaveCardOnClickListener = onClickListener;
+    }
+
+    @NonNull
+    @Override
+    public LeaveCardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        SingleLeaveCardBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.single_leave_card, parent, false);
+        return new LeaveCardViewHolder(binding, leaveCardOnClickListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull LeaveCardViewHolder holder, int position) {
+        holder.setData(leaveList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        if (leaveList == null) {
+            return 0;
+        }
+        return leaveList.size();
+    }
+
+    public static class LeaveCardViewHolder extends RecyclerView.ViewHolder {
+
+        private final SingleLeaveCardBinding binding;
+        private final SingleLeaveCardOnClickListener onClickListener;
+
+        public LeaveCardViewHolder(@NonNull SingleLeaveCardBinding binding, SingleLeaveCardOnClickListener onClickListener) {
+            super(binding.getRoot());
+
+            this.binding = binding;
+            this.onClickListener = onClickListener;
+        }
+
+        public void setData(Leave leave) {
+            binding.setLeave(leave);
+
+            binding.getRoot().setOnClickListener(view -> onClickListener.onClickCard(leave));
+        }
+    }
+
+    public interface SingleLeaveCardOnClickListener {
+
+        void onClickCard(Leave leave);
+    }
+}
--- app/src/main/java/com/flaxeninfosoft/guptaoffset/views/employee/fragments/EmployeeAddClientFragment.java
+++ app/src/main/java/com/flaxeninfosoft/guptaoffset/views/employee/fragments/EmployeeAddClientFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -36,7 +37,7 @@ public class EmployeeAddClientFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_employee_add_client, container, false);
         binding.setClient(new Client());
