4ea815e7a122474515cbafe2bda93b850a29a94c
Pankaj Bairwa <bairwapankaj25@gmail.com>
2022-12-26 15:55:01 +0530

    Implemented EmployeeAddClientFragment.java Refactored fragment_employee_add_client.xml.

--- app/src/main/java/com/flaxeninfosoft/guptaoffset/views/employee/fragments/EmployeeAddClientFragment.java
+++ app/src/main/java/com/flaxeninfosoft/guptaoffset/views/employee/fragments/EmployeeAddClientFragment.java
@@ -1,5 +1,6 @@
 package com.flaxeninfosoft.guptaoffset.views.employee.fragments;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,17 +9,21 @@ import android.widget.Toast;
 
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import com.flaxeninfosoft.guptaoffset.R;
 import com.flaxeninfosoft.guptaoffset.databinding.FragmentEmployeeAddClientBinding;
 import com.flaxeninfosoft.guptaoffset.models.Client;
 import com.flaxeninfosoft.guptaoffset.viewModels.EmployeeViewModel;
 
-public class EmployeeAddClientFragment extends Fragment{
+public class EmployeeAddClientFragment extends Fragment {
 
     private FragmentEmployeeAddClientBinding binding;
     private EmployeeViewModel viewModel;
+
+    private ProgressDialog progressDialog;
 
     public EmployeeAddClientFragment() {
         // Required empty public constructor
@@ -34,19 +39,34 @@ public class EmployeeAddClientFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding= DataBindingUtil.inflate(inflater,R.layout.fragment_employee_add_client,container,false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_employee_add_client, container, false);
         binding.setClient(new Client());
 
-        viewModel.getToastMessageLiveData().observe(getViewLifecycleOwner(),this::toastMessage);
-        binding.empolyeeAddClientRegisterBtn.setOnClickListener(this::registerClient);
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setTitle("Adding Expense..");
+        progressDialog.setMessage("Please wait.\n Adding expenses");
+        progressDialog.setCancelable(false);
+
+
+        binding.employeeAddClientRegisterBtn.setOnClickListener(this::registerClient);
 
         return binding.getRoot();
     }
 
     private void registerClient(View view) {
-        if(isValidateForm()){
-            //TODO
+        if (isValidateForm()) {
+            progressDialog.show();
+            viewModel.addClient(binding.getClient()).observe(getViewLifecycleOwner(), isSuccessful -> {
+                progressDialog.dismiss();
+                if (isSuccessful){
+                    navigateUp();
+                }
+            });
         }
+    }
+
+    private void navigateUp() {
+        Navigation.findNavController(binding.getRoot()).navigateUp();
     }
 
     private boolean isValidateForm() {
@@ -57,28 +77,25 @@ public class EmployeeAddClientFragment extends Fragment{
             return false;
         }
         if (binding.getClient().getOrgName().isEmpty()) {
-            binding.employeeAddClientOrganisationName.setError("Address is required");
+            binding.employeeAddClientOrganisationName.setError("Organization name is required");
             binding.employeeAddClientOrganisationName.requestFocus();
             return false;
         }
         if (binding.getClient().getAddress().isEmpty()) {
-            binding.employeeAddClientAddress.setError("Domain is required");
+            binding.employeeAddClientAddress.setError("Address is required");
             binding.employeeAddClientAddress.requestFocus();
-
             return false;
         }
-        if (binding.getClient().getContactNo().toString().isEmpty()) {
-            binding.employeeAddClientContact.setError("SubDomain is required");
+        if (binding.getClient().getContactNo().isEmpty()) {
+            binding.employeeAddClientContact.setError("Contact number is required");
             binding.employeeAddClientContact.requestFocus();
+            return false;
+        }
+        if (binding.getClient().getLatitude() == 0d || binding.getClient().getLongitude() == 0d) {
+            Toast.makeText(getContext(), "Fetching location, Please wait", Toast.LENGTH_SHORT).show();
             return false;
         }
         return true;
 
-    }
-
-    private void toastMessage(String s) {
-        if (s != null && !s.isEmpty()) {
-            Toast.makeText(getContext(), s, Toast.LENGTH_SHORT).show();
-        }
     }
 }
