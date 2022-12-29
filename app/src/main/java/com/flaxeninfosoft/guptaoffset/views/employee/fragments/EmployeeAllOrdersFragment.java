ade7600d86bbb1094d94fd5f48c6e45f6a389a6b
Pankaj Bairwa <bairwapankaj25@gmail.com>
2022-12-29 15:40:26 +0530

    Added EmployeeAllOrdersFragment.

---
+++ app/src/main/java/com/flaxeninfosoft/guptaoffset/views/employee/fragments/EmployeeAllOrdersFragment.java
@@ -0,0 +1,31 @@
+package com.flaxeninfosoft.guptaoffset.views.employee.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.flaxeninfosoft.guptaoffset.R;
+
+public class EmployeeAllOrdersFragment extends Fragment {
+
+    public EmployeeAllOrdersFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_employee_all_orders, container, false);
+    }
+}
