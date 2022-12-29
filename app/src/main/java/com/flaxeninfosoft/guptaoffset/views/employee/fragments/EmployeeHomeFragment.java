2f98a057e4b54f31323212b5025160479c88230b
Pankaj Bairwa <bairwapankaj25@gmail.com>
2022-12-29 11:37:22 +0530

    Bugfix in EmployeeHomeFragment. Bugfix in EmployeeAddExpenseFragment.java

--- app/src/main/java/com/flaxeninfosoft/guptaoffset/views/employee/fragments/EmployeeHomeFragment.java
+++ app/src/main/java/com/flaxeninfosoft/guptaoffset/views/employee/fragments/EmployeeHomeFragment.java
@@ -19,7 +19,6 @@ public class EmployeeHomeFragment extends Fragment {
 
     private EmployeeViewModel viewModel;
     private FragmentEmployeeHomeBinding binding;
-    private Boolean isVisible;
 
     public EmployeeHomeFragment() {
         // Required empty public constructor
@@ -28,7 +27,6 @@ public class EmployeeHomeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        isVisible = false;
         viewModel = new ViewModelProvider.AndroidViewModelFactory(getActivity().getApplication()).create(EmployeeViewModel.class);
     }
 
@@ -49,12 +47,11 @@ public class EmployeeHomeFragment extends Fragment {
         binding.employeeHomeCardClients.setOnClickListener(this::navigateToClients);
 
         binding.employeeHomeViewFab.setOnClickListener(view -> {
-            if (isVisible) {
+            if (binding.employeeHomeCard.getVisibility() == View.VISIBLE) {
                 binding.employeeHomeCard.setVisibility(View.GONE);
             } else {
                 binding.employeeHomeCard.setVisibility(View.VISIBLE);
             }
-            isVisible = !isVisible;
         });
 
         return binding.getRoot();
