f8ebd1e63cc82e4faa3198203485fe6c0e8fa355
Arun Chouhan <arunchouhan2353@gmail.com>
2023-01-03 13:05:39 +0530

    Implement EmployeeClientProfileFragment.java

--- app/src/main/java/com/flaxeninfosoft/guptaoffset/views/employee/fragments/EmployeeClientProfileFragment.java
+++ app/src/main/java/com/flaxeninfosoft/guptaoffset/views/employee/fragments/EmployeeClientProfileFragment.java
@@ -1,11 +1,13 @@
 package com.flaxeninfosoft.guptaoffset.views.employee.fragments;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
@@ -15,6 +17,8 @@ import android.view.ViewGroup;
 import com.flaxeninfosoft.guptaoffset.R;
 import com.flaxeninfosoft.guptaoffset.databinding.FragmentEmployeeAllLeavesBinding;
 import com.flaxeninfosoft.guptaoffset.databinding.FragmentEmployeeClientProfileBinding;
+import com.flaxeninfosoft.guptaoffset.models.Client;
+import com.flaxeninfosoft.guptaoffset.models.Order;
 import com.flaxeninfosoft.guptaoffset.viewModels.EmployeeViewModel;
 
 //TODO
@@ -22,9 +26,9 @@ public class EmployeeClientProfileFragment extends Fragment {
 
     private FragmentEmployeeClientProfileBinding binding;
     private EmployeeViewModel viewModel;
+    private ProgressDialog progressDialog;
 
     public EmployeeClientProfileFragment() {
-        // Required empty public constructor
     }
 
     @Override
@@ -37,8 +41,27 @@ public class EmployeeClientProfileFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_employee_client_profile, container, false);
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setTitle("Fetching data...");
+        progressDialog.setMessage("Loading");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        Long clientId=getArguments().getLong(getString(R.string.key_client_id));
+
+        viewModel.getClientById(clientId).observe(getViewLifecycleOwner(),this::setClintDetails);
 
         return binding.getRoot();
+    }
+
+    private void setClintDetails(Client client) {
+        progressDialog.dismiss();
+        if (client == null) {
+            Navigation.findNavController(binding.getRoot()).navigateUp();
+        }
+        else{
+            binding.setClient(client);
+        }
     }
 
 }
