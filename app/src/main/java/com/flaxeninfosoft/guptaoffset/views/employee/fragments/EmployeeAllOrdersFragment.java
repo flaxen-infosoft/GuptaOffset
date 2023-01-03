f00294bb52de985ff33c95c184d0bf5c442c8ddc
Pankaj Bairwa <bairwapankaj25@gmail.com>
2023-01-03 12:49:48 +0530

    Update nav_main_employee.xml. Added and Implemented EmployeeLeaveProfile. Added fragment_employee_leave_profile.xml. Implemented EmployeeAllOrdersFragment. Additions in strings.xml.

--- app/src/main/java/com/flaxeninfosoft/guptaoffset/views/employee/fragments/EmployeeAllOrdersFragment.java
+++ app/src/main/java/com/flaxeninfosoft/guptaoffset/views/employee/fragments/EmployeeAllOrdersFragment.java
@@ -1,18 +1,18 @@
 package com.flaxeninfosoft.guptaoffset.views.employee.fragments;
 
 import android.os.Bundle;
-
-import androidx.databinding.DataBindingUtil;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+
 import com.flaxeninfosoft.guptaoffset.R;
 import com.flaxeninfosoft.guptaoffset.adapters.OrderRecyclerAdapter;
-import com.flaxeninfosoft.guptaoffset.databinding.FragmentEmployeeAddOrderBinding;
 import com.flaxeninfosoft.guptaoffset.databinding.FragmentEmployeeAllOrdersBinding;
 import com.flaxeninfosoft.guptaoffset.models.Order;
 import com.flaxeninfosoft.guptaoffset.viewModels.EmployeeViewModel;
@@ -31,24 +31,26 @@ public class EmployeeAllOrdersFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        viewModel=new ViewModelProvider.AndroidViewModelFactory(getActivity().getApplication()).create(EmployeeViewModel.class);
+        viewModel = new ViewModelProvider.AndroidViewModelFactory(getActivity().getApplication()).create(EmployeeViewModel.class);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding= DataBindingUtil.inflate(inflater,R.layout.fragment_employee_all_orders,container,false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_employee_all_orders, container, false);
 
-        viewModel.getCurrentEmployeeOrders().observe(getViewLifecycleOwner(),this::updateOrdersList);
+        viewModel.getCurrentEmployeeOrders().observe(getViewLifecycleOwner(), this::updateOrdersList);
 
-        return  binding.getRoot();
+        return binding.getRoot();
     }
 
     private void updateOrdersList(List<Order> orders) {
-        OrderRecyclerAdapter adapter=new OrderRecyclerAdapter(orders, new OrderRecyclerAdapter.SingleOrderCardOnClickListener() {
+        OrderRecyclerAdapter adapter = new OrderRecyclerAdapter(orders, new OrderRecyclerAdapter.SingleOrderCardOnClickListener() {
             @Override
             public void onCLickCard(Order order) {
-                //TODO
+                Bundle bundle = new Bundle();
+                bundle.putLong(getString(R.string.key_leave_id), order.getId());
+                Navigation.findNavController(binding.getRoot()).navigate(R.id.action_employeeAllOrdersFragment_to_employeeOrderDetailsFragment, bundle);
             }
         });
         binding.employeeOrderListRecycler.setAdapter(adapter);
