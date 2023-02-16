50d40866e630a718eed5c4014b291716aaca6430
Pankaj Bairwa <bairwapankaj25@gmail.com>
2023-02-17 03:17:53 +0530

    Added and implemented layout_dropdown.xml. Added firm in Constants, Employee, fragment_admin_add_super_employee.xml, SuperEmployeeAddEmployeeFragment, AdminAddSuperEmployeeFragment

--- app/src/main/java/com/flaxeninfosoft/guptaoffset/views/admin/fragments/AdminAddSuperEmployeeFragment.java
+++ app/src/main/java/com/flaxeninfosoft/guptaoffset/views/admin/fragments/AdminAddSuperEmployeeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -15,7 +16,11 @@ import androidx.navigation.Navigation;
 import com.flaxeninfosoft.guptaoffset.R;
 import com.flaxeninfosoft.guptaoffset.databinding.FragmentAdminAddSuperEmployeeBinding;
 import com.flaxeninfosoft.guptaoffset.models.Employee;
+import com.flaxeninfosoft.guptaoffset.utils.Constants;
 import com.flaxeninfosoft.guptaoffset.viewModels.AdminViewModel;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class AdminAddSuperEmployeeFragment extends Fragment {
 
@@ -37,6 +42,10 @@ public class AdminAddSuperEmployeeFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_admin_add_super_employee, container, false);
         binding.setEmployee(new Employee());
+
+        binding.adminAddSuperEmployeeFirmAutocomplete.setAdapter(
+                new ArrayAdapter<>(getContext(), R.layout.layout_dropdown, Constants.FIRMS)
+        );
 
         binding.adminAddSuperEmployeeBtn.setOnClickListener(this::onClickAddButton);
 
@@ -66,7 +75,64 @@ public class AdminAddSuperEmployeeFragment extends Fragment {
     }
 
     private boolean isValidInput() {
-        //TODO add validation
+        Employee employee = binding.getEmployee();
+
+        if (employee.getName() == null || employee.getName().trim().isEmpty()){
+            Toast.makeText(getContext(), "Name required", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (employee.getEmail() == null || employee.getEmail().trim().isEmpty()){
+            Toast.makeText(getContext(), "Email required", Toast.LENGTH_SHORT).show();;
+            return false;
+        }
+
+        if (employee.getPassword() == null || employee.getPassword().trim().isEmpty()){
+            Toast.makeText(getContext(), "Password required", Toast.LENGTH_SHORT).show();;
+            return false;
+        }
+
+        if (employee.getArea() == null || employee.getArea().trim().isEmpty()){
+            Toast.makeText(getContext(), "Area required", Toast.LENGTH_SHORT).show();;
+            return false;
+        }
+
+        if (employee.getDailyAllowance() == null || employee.getDailyAllowance().trim().isEmpty()){
+            Toast.makeText(getContext(), "Daily allowance required", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        try {
+            int da = Integer.parseInt(employee.getDailyAllowance());
+            if (da<0){
+                throw new Exception();
+            }
+        }catch (Exception e){
+            Toast.makeText(getContext(), "Enter valid daily allowance", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (employee.getSalary() == null || employee.getSalary().trim().isEmpty()){
+            Toast.makeText(getContext(), "Salary required", Toast.LENGTH_SHORT).show();;
+            return false;
+        }
+
+        try {
+            int da = Integer.parseInt(employee.getSalary());
+            if (da<0){
+                throw new Exception();
+            }
+        }catch (Exception e){
+            Toast.makeText(getContext(), "Enter valid Salary", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        List<String> firms = Arrays.asList(Constants.FIRMS);
+        if (!firms.contains(employee.getFirm().trim().toUpperCase())){
+            Toast.makeText(getContext(), "Enter valid firm", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
         return true;
     }
 }
