6dae13d4216f9b83990b732640f912e419ee2b6c
Akshat dale <akshatdale1611@gmail.com>
2023-06-23 16:47:46 +0530

    changes in adapter for set data for accounting completed

--- app/src/main/java/com/flaxeninfosoft/guptaoffset/adapters/DailyReportRecyclerAdapter.java
+++ app/src/main/java/com/flaxeninfosoft/guptaoffset/adapters/DailyReportRecyclerAdapter.java
@@ -8,31 +8,71 @@ import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.flaxeninfosoft.guptaoffset.R;
+import com.flaxeninfosoft.guptaoffset.databinding.ReportingCompletedLayoutBinding;
 import com.flaxeninfosoft.guptaoffset.databinding.SingleDailyReportCardBinding;
 import com.flaxeninfosoft.guptaoffset.models.Eod;
 
 import java.util.List;
 
-public class DailyReportRecyclerAdapter extends RecyclerView.Adapter<DailyReportRecyclerAdapter.DailyReportViewHolder> {
+public class DailyReportRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private final List<Eod> eodList;
-    private final SingleDailyReportOnClickListener listener;
+    private final SingleDailyReportOnClickListener singleDailyReportOnClickListener;
+
+    private static final int VIEW_TYPE_LAYOUT1 = 1;
+    private static final int VIEW_TYPE_LAYOUT2 = 2;
 
     public DailyReportRecyclerAdapter(List<Eod> eodList, SingleDailyReportOnClickListener listener) {
         this.eodList = eodList;
-        this.listener = listener;
+        this.singleDailyReportOnClickListener = listener;
     }
 
-    @NonNull
+
     @Override
-    public DailyReportViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        SingleDailyReportCardBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.single_daily_report_card, parent, false);
-        return new DailyReportRecyclerAdapter.DailyReportViewHolder(binding, listener);
+    public int getItemViewType(int position) {
+        String type = eodList.get(position).getType();
+
+        if (type.equals("1")) {
+            return VIEW_TYPE_LAYOUT2;
+        } else {
+            return VIEW_TYPE_LAYOUT1;
+        }
+    }
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        SingleDailyReportCardBinding singleDailyReportCardBinding;
+        ReportingCompletedLayoutBinding reportingCompletedLayoutBinding;
+        switch (viewType) {
+            case VIEW_TYPE_LAYOUT1:
+                singleDailyReportCardBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.single_daily_report_card, parent, false);
+                return new DailyReportRecyclerAdapter.DailyReportViewHolder(singleDailyReportCardBinding, singleDailyReportOnClickListener);
+            case VIEW_TYPE_LAYOUT2:
+                reportingCompletedLayoutBinding = DataBindingUtil.inflate(inflater, R.layout.reporting_completed_layout, parent, false);
+                return new DailyReportRecyclerAdapter.ReportCompleteViewHolder(reportingCompletedLayoutBinding);
+            default:
+                throw new IllegalArgumentException("Invalid view type: " + viewType);
+        }
+
     }
 
     @Override
-    public void onBindViewHolder(@NonNull DailyReportViewHolder holder, int position) {
-        holder.setData(eodList.get(position));
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+        switch (holder.getItemViewType()) {
+            case VIEW_TYPE_LAYOUT1:
+                DailyReportViewHolder dailyReportViewHolder = (DailyReportViewHolder) holder;
+                dailyReportViewHolder.setData(eodList.get(position));
+                break;
+            case VIEW_TYPE_LAYOUT2:
+                ReportCompleteViewHolder reportCompleteViewHolder = (ReportCompleteViewHolder) holder;
+                reportCompleteViewHolder.setData(eodList.get(position));
+                break;
+        }
+
     }
 
     @Override
@@ -68,6 +108,20 @@ public class DailyReportRecyclerAdapter extends RecyclerView.Adapter<DailyReport
 
 //            Yeah Boiii!!!
             binding.dailyReportTa.setText(ta + "");
+        }
+    }
+
+    public static class ReportCompleteViewHolder extends RecyclerView.ViewHolder {
+
+        ReportingCompletedLayoutBinding reportingCompletedLayoutBinding;
+
+        public ReportCompleteViewHolder(@NonNull ReportingCompletedLayoutBinding reportingCompletedLayoutBinding) {
+            super(reportingCompletedLayoutBinding.getRoot());
+            this.reportingCompletedLayoutBinding = reportingCompletedLayoutBinding;
+        }
+
+        public void setData(Eod eod) {
+            reportingCompletedLayoutBinding.setEod(eod);
         }
     }
 
